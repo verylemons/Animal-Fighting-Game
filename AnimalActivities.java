015d7c5e48e1e7329e0d0deb5cb7f9a2fee45d85
Kwae Htoo <116234889+verylemons@users.noreply.github.com>
2023-09-27 12:28:03 -0700

    AnimalActivities.java

--- AnimalActivities.java
+++ AnimalActivities.java
@@ -1,21 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   ALL STUDENTS COMPLETE THESE SECTIONS
-// Main Class File:    Animal
-// File:               AnimalActivities
-// Quarter:            CSE 8B Spring 2023
-//
-// Author:             Kwae Htoo
-// Instructor's Name:  Professor Ochoa
-//
-///////////////////////////////////////////////////////////////////////////////
-//                   STUDENTS WHO GET HELP COMPLETE THIS SECTION
-//                   You must fully acknowledge and credit all sources of help,
-//                   other than Instructors and TAs.
-//
-// Persons:          N/A
-//
-// Online sources:   N/A
-//////////////////////////// 80 columns wide //////////////////////////////////
 
 /**
  * Class for preforming activites for animals
